5562510919020513cad5f363e6aa862799f39fd6
NishadDamale <nishaddamale1010@gmail.com>
2026-04-11 13:39:43 +0530

    Add first-visit popup and disease labels in prediction dashboard

--- backend/src/routes/predict.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { predict, predictFromImage } = require("../controllers/predict.controller");
-const authMiddleware = require("../middleware/auth.middleware");
-
-router.post("/", authMiddleware, predict);
-router.post("/image", authMiddleware, predictFromImage);
-
-module.exports = router;
